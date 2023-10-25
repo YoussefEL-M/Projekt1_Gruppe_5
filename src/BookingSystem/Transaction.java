b5907597bd1de0e4ae1bde970b2d9c6346db0c79
YoussefEL-M <yoel0001@stud.kea.dk>
2023-10-25 13:38:49 +0200

    New and improved

--- src/BookingSystem/Transaction.java
+++ src/BookingSystem/Transaction.java
@@ -1,5 +1,25 @@
 package BookingSystem;
-
+import java.util.*;
 public class Transaction{
+    
+    private double amount;
+    private boolean paymentReceived = false;
 
+    public void setAmount(double amount){
+        this.amount=amount;
+    }
+    public boolean setPaymentReceived(boolean paymentReceived){
+        this.paymentReceived = paymentReceived;
+        return paymentReceived;
+    }
+
+    public String toString(){
+        String status;
+        if (paymentReceived) {
+            status = "Received";
+        } else {
+            status = "Not Received";
+        }
+        return "Booking: 1"  + ", Amount: " + amount + ", Payment: " + status;
+    }
 }
