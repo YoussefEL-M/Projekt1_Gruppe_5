a8e156510e1b252ed0a3e94a17e96fea2a7c9095
ViggoBeck <vibe0002@stud.kea.dk>
2023-10-25 14:00:35 +0200

    Second push

--- src/BookingSystem/Salon.java
+++ src/BookingSystem/Salon.java
@@ -1,7 +1,23 @@
 package BookingSystem;
 
+import java.util.ArrayList;
+
 public class Salon {
     public static void main(String[] args) {
+
+        ArrayList<Booking> list=new ArrayList<>();
+        int openingTime;
+        int closingTime;
+
+    }
+    public void printBooking() {
+
+    }
+
+    public void opening(int openingTime){
+
+    }
+    public void closing(int closingTime) {
 
     }
 }
