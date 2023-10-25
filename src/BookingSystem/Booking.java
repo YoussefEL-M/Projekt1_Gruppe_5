e48352bf841351111ff8d99a36e6538ae39ad362
tgavri <thom54s8@stud.kea.dk>
2023-10-25 23:21:33 +0200

    Update, incl. createBooking
    @Viggo, please add your work into Salon class.

--- src/BookingSystem/Booking.java
+++ src/BookingSystem/Booking.java
@@ -1,7 +1,40 @@
 package BookingSystem;
 
-public class Booking {
-    public static void main(String[] args) {
+import java.awt.print.Book;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Booking {
+    String name;
+    double time; //skal vi have duration med, eller kører vi med 30min intervaller? Hvis ja, indsæt.
+    LocalDate date;
+
+    Booking(String name, double time, LocalDate date){
+        this.name=name;
+        this.time=time;
+        this.date=date;
     }
+
+    public void createBooking(ArrayList<Double> bookingList){
+        /* int interval = 30; //30 min.
+        int startTime = 10 * 60; // 10:00 converted to minutes
+        int endTime = 18 * 60; // 18:00 converted to minutes
+        int timeInMinutes = (int) (time * 60);
+
+        Ikke nødvendig, da arraylist i Salon har available times.
+*/
+        if (bookingList.contains(time)) {
+            System.out.println("Booking created for " + name + " on " + date + " at time " + time +" O'Clock.");
+            bookingList.remove(time);
+        }
+        else{
+            System.out.println("The selected time is already booked. Please choose another time.");
+        }
+    }
+    /* første input, kan slettes.
+    private boolean isValidTime(double time) {
+        return time >= 10 && time <= 18;
+    }
+    */
+
 }
--- src/BookingSystem/Salon.java
+++ src/BookingSystem/Salon.java
@@ -2,8 +2,33 @@ package BookingSystem;
 
 import java.util.ArrayList;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Salon {
-    public static void main(String[] args) {
+    private ArrayList<Double> bookingList = new ArrayList<>();
+
+    public Salon() {
+        this.bookingList = new ArrayList<>();
+
+    }
+        public static void main(String[] args) {
+            ArrayList<Double> bookingList = new ArrayList<>();
+            for (double hour = 10; hour < 18; hour++) {
+                bookingList.add(hour);
+                bookingList.add(hour + 0.5); // Add 30 minutes
+            }
+
+
+            Salon salon = new Salon();
+            salon.bookingList = bookingList;
+
+            // test
+            Booking a = new Booking("John Jensen", 12.5, LocalDate.now());
+            a.createBooking(salon.bookingList);
+
+
+            System.out.println("Avaliable Booking List times: " + salon.bookingList);
 
         ArrayList<Booking> list=new ArrayList<>();
         int openingTime;
@@ -20,4 +45,6 @@ public class Salon {
     public void closing(int closingTime) {
 
     }
+
+
 }
