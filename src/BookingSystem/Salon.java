2a1179c73b8d05342e7b76b92492484b9c64a19f
ViggoBeck <vibe0002@stud.kea.dk>
2023-10-26 08:59:07 +0200

    Second push

--- src/BookingSystem/Salon.java
+++ src/BookingSystem/Salon.java
@@ -1,23 +1,72 @@
 package BookingSystem;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Salon {
+    ArrayList<Booking> bookingList;
+    int openingTime;
+    int closingTime;
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        ArrayList<Booking> list=new ArrayList<>();
-        int openingTime;
-        int closingTime;
+
+        do {
+            System.out.println("Welcome to Harry's Salon!");
+            System.out.println("Hairy?");
+            System.out.println("See Cotter!");
+            System.out.println("1. Book appointment");
+            System.out.println("2. Cancel appointment");
+            System.out.println("3. Add product");
+            System.out.println("4. View services");
+            System.out.println("5. View finances");
+            System.out.println("6. Exit");
+            System.out.print("Please enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            String message;
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    message = "Thanks for using our salon booking system. Goodbye!";
+                    break;
+                default:
+                    message = "Error. Invalid input. Try again";
+                    break;
+
+            }
+        }while (choice !=4);
+        scanner.close();
+
 
     }
-    public void printBooking() {
 
+    public void Bookings() {
+        bookingList = new ArrayList<>();
+    }
+    public void printBooking() {
     }
 
     public void opening(int openingTime){
+        this.openingTime = openingTime;
+
 
     }
     public void closing(int closingTime) {
+        this.closingTime = closingTime;
 
     }
+
 }
