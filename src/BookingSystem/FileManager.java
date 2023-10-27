66d8268703c9de1c6022a0753d7c09276381a8ee
YoussefEL-M <yoel0001@stud.kea.dk>
2023-10-27 13:10:46 +0200

    Up to date

--- src/BookingSystem/FileManager.java
+++ src/BookingSystem/FileManager.java
@@ -17,76 +17,79 @@ public class FileManager {
         saveBookings(list);
     }
 
-    static ArrayList<Booking> getBookings(String fileName) throws IOException {
+    static ArrayList<Booking> getBookings(String fileName) {
         ArrayList<Booking> list = new ArrayList<>();
 
-        FileReader file = new FileReader(fileName+".txt");
-        BufferedReader in = new BufferedReader(file);
-        String line = in.readLine();
+        try (BufferedReader in = new BufferedReader((new FileReader(fileName + ".txt")))) {
+            String line = in.readLine();
 
-        while(line!=null){
-            String[] bits = line.split(",");
-            String name = bits[0];
-            String note = bits[1];
-            LocalDate date = LocalDate.parse(bits[2]);
-            LocalTime time = LocalTime.parse(bits[3]);
-            double amount = Double.parseDouble(bits[4]);
-            boolean paymentReceived = Boolean.getBoolean(bits[5]);
+            while (line != null) {
+                String[] bits = line.split(",");
+                String name = bits[0];
+                String note = bits[1];
+                LocalDate date = LocalDate.parse(bits[2]);
+                LocalTime time = LocalTime.parse(bits[3]);
+                double amount = Double.parseDouble(bits[4]);
+                boolean paymentReceived = Boolean.parseBoolean(bits[5]);
 
-            list.add(new Booking(name,note,date,time,amount,paymentReceived));
-            line = in.readLine();
-        } //while
-        file.close();
+                list.add(new Booking(name, note, date, time, amount, paymentReceived));
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return list;
     } //getBookings
 
     static void saveBookings(ArrayList<Booking> list) throws IOException {
-        ArrayList<Booking> secondList = getBookings("PastBookings");
+        try (PrintWriter out = new PrintWriter(new FileWriter("Bookings.txt"));
+             PrintWriter secondOut = new PrintWriter(new FileWriter("PastBookings.txt"))) {
 
-        FileWriter file = new FileWriter("Bookings.txt");
-        FileWriter secondFile = new FileWriter("PastBookings.txt");
-        PrintWriter out = new PrintWriter(file);
-        PrintWriter secondOut = new PrintWriter(secondFile);
+            ArrayList<Booking> secondList = getBookings("PastBookings");
 
-        for(Booking b: secondList){
-            secondOut.println(b.name+","+b.note+","+b.date+","+b.time+","+b.transaction.getAmount()+","+b.transaction.getPaymentReceived());
+            for (Booking b : secondList) {
+                secondOut.println(b.name + "," + b.note + "," + b.date + "," + b.time + "," + b.transaction.getAmount() + "," + b.transaction.getPaymentReceived());
+            }
+
+            for (Booking b : list) {
+                if (b.date.isBefore(LocalDate.now())) {
+                    secondOut.println(b.name + "," + b.note + "," + b.date + "," + b.time + "," + b.transaction.getAmount() + "," + b.transaction.getPaymentReceived());
+                } else {
+                    out.println(b.name + "," + b.note + "," + b.date + "," + b.time + "," + b.transaction.getAmount() + "," + b.transaction.getPaymentReceived());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static ArrayList<LocalDate> getClosedDays() {
+        ArrayList<LocalDate> list = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader("ClosedDays.txt"))) {
+            String line = in.readLine();
+
+            while (line != null) {
+                LocalDate date = LocalDate.parse(line);
+                list.add(date);
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); 
         }
 
-        for(Booking b: list){
-            if(b.date.isBefore(LocalDate.now())){
-                secondOut.println(b.name+","+b.note+","+b.date+","+b.time+","+b.transaction.getAmount()+","+b.transaction.getPaymentReceived());
-            }
-            else{
-                out.println(b.name+","+b.note+","+b.date+","+b.time+","+b.transaction.getAmount()+","+b.transaction.getPaymentReceived());
-            }
-        } //for
-        file.close();
-        secondFile.close();
-    } //saveBookings
-
-    static ArrayList<LocalDate> getClosedDays() throws IOException {
-        ArrayList<LocalDate> list = new ArrayList<>();
-        FileReader file = new FileReader("ClosedDays.txt");
-        BufferedReader in = new BufferedReader(file);
-        String line = in.readLine();
-
-        while(line!=null){
-            LocalDate date = LocalDate.parse(line);
-            list.add(date);
-            line = in.readLine();
-        } //while
-        file.close();
         return list;
     } //getClosedDays
 
-    static void saveClosedDays(ArrayList<LocalDate> list) throws IOException {
-        FileWriter file = new FileWriter("ClosedDays.txt");
-        PrintWriter out = new PrintWriter(file);
-
-        for(LocalDate d: list){
-            if(!d.isBefore(LocalDate.now()))
-                out.println(d);
+    static void saveClosedDays(ArrayList<LocalDate> list) {
+        try (PrintWriter out = new PrintWriter(new FileWriter("ClosedDays.txt"))) {
+            for (LocalDate d : list) {
+                if (!d.isBefore(LocalDate.now()))
+                    out.println(d);
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception properly (log it, show a message, etc.)
         }
-        file.close();
     } //saveClosedDays
 } //class
