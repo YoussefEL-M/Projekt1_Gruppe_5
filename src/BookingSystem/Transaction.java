79ed0c556074455799ee11165ec4d38c5413a38c
TipJay <severin.frouvne@gmail.com>
2023-10-30 11:26:01 +0100

    Added addAmount() to Transaction.

--- src/BookingSystem/Transaction.java
+++ src/BookingSystem/Transaction.java
@@ -23,6 +23,9 @@ public class Transaction{
     public boolean getPaymentReceived(){
         return this.paymentReceived;
     }
+    public void addAmount(double amount){
+        this.amount = this.amount+amount;
+    }
 
     public String toString(){
         String status;
